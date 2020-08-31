fdb940023ec70257e0eb93107709f3bba2ee99c6
jncarreiro <jncarreiro@umass.edu>
2020-08-31 14:25:01 +0000

    added files (html and R code) for lab 1 to github. used as practice, lab1 was basic R skills.

---
+++ lab1_jlb.R
@@ -0,0 +1,29 @@
+# to use calculator you can type 3+3 here then shift enter or
+# write it after the blue arror in the control then enter 
+
+## Exercise 1
+x <- 3
+y <- 15
+# sum
+x+y
+# difference
+x-y
+# product 
+x*y
+# quotient
+x/y
+
+## Exercise 2
+x <- 2
+y <- 3
+# x^5 is greater than y^4 and return logical value
+(x^5) > (y^4) # returned FALSE
+
+## Exercise 3 
+v <- c(22,62,148,43,129)
+sum(v) #404
+v/(sum(v))
+
+## Exercise 4 
+codons <- c('A','T','C','G')
+sort(codons)
